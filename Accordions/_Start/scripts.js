0728338957a82155e8c354f402571d4eeafa5db4
taramouse <support@optimized4u.org>
2017-10-15 15:15:28 +0100

    Added is-open class toggle to change button icon.

--- Accordions/_Start/scripts.js
+++ Accordions/_Start/scripts.js
@@ -1,8 +1,9 @@
-var accordions = document.getElementsByClassName("accordion");
+var accordions = document.getElementsByClassName('accordion');
 
 for (var i = 0; i < accordions.length; i++) {
 
   accordions[i].onclick = function () {
+    this.classList.toggle('is-open');
     // select the next element down the DOM to get the accordion content element
     var content = this.nextElementSibling;
     // determine if content element is open or closed
@@ -11,7 +12,7 @@ for (var i = 0; i < accordions.length; i++) {
       content.style.maxHeight = null;
     } else {
       // accordion is closed, scrollHeight is the total size of the elements content
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = content.scrollHeight + 'px';
 
     }
   }
