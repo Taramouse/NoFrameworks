b8ce4132a0236aac226eace7567cec9358e01220
taramouse <support@optimized4u.org>
2017-10-15 14:53:57 +0100

    Added code to open and close accordion content.

---
+++ Accordions/_Start/scripts.js
@@ -0,0 +1,18 @@
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+
+  accordions[i].onclick = function () {
+    // select the next element down the DOM to get the accordion content element
+    var content = this.nextElementSibling;
+    // determine if content element is open or closed
+    if (content.style.maxHeight) {
+      // accordion is open, close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is closed, scrollHeight is the total size of the elements content
+      content.style.maxHeight = content.scrollHeight + "px";
+
+    }
+  }
+}
